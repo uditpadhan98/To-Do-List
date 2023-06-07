036d7dc4f6c4730d8192d8c0900c6966a994c81e
Udit Padhan <uditpadhan98@gmail.com>
2023-06-07 20:22:49 +0530

    new change

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 const erase=document.querySelector("#eraser");
+
 const item=document.querySelector("#Item");
 const user=document.querySelector("#user");
 
+
 erase.addEventListener("click",()=>{
-    item.textContent="";
+  item.textContent="";
+  localStorage.clear();
 })
-  
+
+// const itemArray=[];
+let i=0;
 user.addEventListener("keydown",(event)=>{
-    if(event.key=="Enter")
-      addItem();
+  if(event.key=="Enter")
+  {
+    // itemArray.push(user.value);
+    localStorage.setItem(`item${i}`,JSON.stringify(user.value));  
+    i++;  
+    // localStorage.getItem('item')?JSON.parse(localStorage.getItem('item')):[];
+    addItem();
+  }
 });
-  
+let j=0;
 addItem=()=>{
-    const item=document.createElement("h2")
-    item.textContent=":) "+user.value;
+  let item=document.createElement("h2")
+  const data=JSON.parse(localStorage.getItem(`item${j}`));
+  j++;
+  item.textContent=":) "+data;
+  // for(let i=0;i<itemArray.length;i++){
+  // }
   
-    item.addEventListener("click",()=>{
-      if(item.style.textDecoration!="line-through")
-        item.style.textDecoration="line-through";
-      else
-        item.style.textDecoration="none";
-    })
+  item.addEventListener("click",()=>{
+    if(item.style.textDecoration!="line-through")
+    item.style.textDecoration="line-through";
+    else
+    item.style.textDecoration="none";
+  })
   
-    document.querySelector("#Item").appendChild(item);
-    document.querySelector("#user").value = "";
+  document.querySelector("#Item").appendChild(item);
+  document.querySelector("#user").value = "";
+    
 }
+
